6e96ce0befabe81ed3e033b5b7623b792f6512ce
Beatriz Soares <beatriz.ferreira8silva@gmail.com>
2023-01-24 15:11:34 -0300

    Primeiro commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import cipher from './cipher.js';
 
-document.getElementById(id botao).addEventListener(click, function cifrar)
+document.getElementById('id botao').addEventListener(click, function cifrar)
 
 function cifrar(){
-document.getelementbyid(id input).value
+document.getelementbyid('id input').value
   let msg
 
 console.log(cipher);
