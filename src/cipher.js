5a9c287111ff082c81eff24f51ac8081406f0c89
Beatriz Soares <beatriz.ferreira8silva@gmail.com>
2023-02-01 14:57:04 -0300

    commit salvar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,12 @@
 const cipher = {
-  // ...
-};
+encode: function (offset,string){
 
+for (let i=0; i< string.length; i++){
+  console.log(string[i] ,"Esta na posição:",i) //Quando i for 0 ele vai verificar a letra que ta na posição 0, assim por diante
+
+}
+
+
+}
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import cipher from './cipher.js';
+const textAreaCifrarDecifrar= document.getElementById("cifrar-decifrar")
+const textAreaResultado= document.getElementById("result")
+const offset=document.getElementById("offset")
+const botaoCifrar= document.getElementById("botao-Cifrar")
 
-document.getElementById('id botao').addEventListener(click, function cifrar)
-
+botaoCifrar.addEventListener("click", cifrar)
 function cifrar(){
-document.getelementbyid('id input').value
-  let msg
+  alert("Mensagem Cifrada")
+  const string=textAreaCifrarDecifrar.value
+ const deslocamento=offset.value
+cipher.encode(deslocamento, string)
+textAreaResultado.value = textAreaCifrarDecifrar.value
+}
+const botaoDecifrar= document.getElementById("botao-Decifrar")
+botaoDecifrar.addEventListener("click", decifrar)
+function decifrar(){
+  alert ("Mensagem Decifrada")
 
-console.log(cipher);
+}
